2abe29ead0b657bf7af381283429b11e034892d2
Maksim Kalashnikov <maksim-kalashnikov-2002@mail.ru>
2023-07-06 18:41:42 +0300

    json

--- src/ClientLog.java
+++ src/ClientLog.java
@@ -1,2 +1,32 @@
+import au.com.bytecode.opencsv.CSVWriter;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class ClientLog {
+    private List<int[]> list = new ArrayList();
+
+    public void log(int productNum, int amount) {
+        list.add(new int[]{productNum, amount});
+
+    }
+
+    public void exportAsCSV(FileWriter file) {
+        try (CSVWriter writer = new CSVWriter(file)) {
+            for (int[] x : list) {
+                String[] contactArrayForCsv = new String[x.length];
+                for (int i = 0; i < x.length; i++) {
+                    contactArrayForCsv[i] = Integer.toString(x[i]);
+                }
+                writer.writeNext(contactArrayForCsv);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.simple.JSONObject;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("basket.bin");
+        ClientLog clientLog = new ClientLog();
+        FileWriter file = new FileWriter("log.csv");
         Basket basket = new Basket(new String[]{"Молоко", "Хлеб", "Гречневая крупа"}, new int[]{50, 14, 80});
         basket.addToCart(0, 2);
+        clientLog.log(0, 2);
         basket.addToCart(2, 1);
-        basket.printCart();
-        basket.saveBin(file);
-        Basket basket1 = Basket.loadFromBinFile(file);
-        basket1.printCart();
-
+        clientLog.log(2, 1);
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        try (FileWriter file2 = new FileWriter("basket.json")) {
+            file2.write(gson.toJson(basket));
+            file2.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        clientLog.exportAsCSV(file);
     }
 }
