dba8d477f6c708cbb2a014eac04786286bef0ef2
Maksim Kalashnikov <maksim-kalashnikov-2002@mail.ru>
2023-07-08 22:33:26 +0300

    create configs loading

--- src/Basket.java
+++ src/Basket.java
@@ -1,3 +1,9 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.*;
 
 public class Basket implements java.io.Serializable {
@@ -5,6 +11,9 @@ public class Basket implements java.io.Serializable {
     private String[] products;
     private int[] price;
     private int[] cart;
+    private String save;
+    private String saveFileName;
+    private String saveFileType;
 
     public Basket(String[] products, int[] price) {
         this.products = products;
@@ -12,13 +21,45 @@ public class Basket implements java.io.Serializable {
         cart = new int[price.length];
     }
 
-    public void addToCart(int productNum, int amount) {
+    public void addToCart(int productNum, int amount) throws IOException {
         cart[productNum] += amount;
+        if (save.equals("true")) {
+            switch (saveFileType) {
+                case ("json"):
+                    GsonBuilder builder = new GsonBuilder();
+                    Gson gson = builder.create();
+                    try (FileWriter file2 = new FileWriter(saveFileName)) {
+                        file2.write(gson.toJson(this));
+                        file2.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case ("txt"):
+                    saveTxt(new File(saveFileName));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     public void setCart(int[] cart) {
         this.cart = cart;
     }
+
+    public void setSave(String save) {
+        this.save = save;
+    }
+
+    public void setSaveFileName(String saveFileName) {
+        this.saveFileName = saveFileName;
+    }
+
+    public void setSaveFileType(String saveFileType) {
+        this.saveFileType = saveFileType;
+    }
+
 
     public void printCart() {
         System.out.println("Ваша корзина:");
@@ -49,4 +90,44 @@ public class Basket implements java.io.Serializable {
         return basket;
     }
 
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (String e : products) {
+                out.print(e + "_");
+            }
+            out.println();
+            for (int e : price) {
+                out.print(e + " ");
+            }
+            out.println();
+            for (int e : cart)
+                out.print(e + " ");
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        Basket basket;
+        try (FileReader fr = new FileReader(textFile)) {
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            String[] products = line.split("_");
+            line = reader.readLine();
+            String[] stringPrice = line.split(" ");
+            int[] price = new int[stringPrice.length];
+            for (int i = 0; i < stringPrice.length; i++) {
+                price[i] = Integer.parseInt(stringPrice[i]);
+            }
+            line = reader.readLine();
+            String[] stringCart = line.split(" ");
+            int[] cart = new int[stringCart.length];
+            for (int i = 0; i < stringCart.length; i++) {
+                cart[i] = Integer.parseInt(stringCart[i]);
+            }
+            basket = new Basket(products, price);
+            for (int i = 0; i < products.length; i++) {
+                basket.addToCart(i, cart[i]);
+            }
+        }
+        return basket;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,59 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main {
-    static List<String> configs = new ArrayList<>();
+    public static List<String> configs = new ArrayList<>();
+    public static boolean save = false;
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory. newInstance();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder1 = factory.newDocumentBuilder();
-        Document doc = builder1.parse( new File("shop.xml"));
+        Document doc = builder1.parse(new File("shop.xml"));
         Node root = doc.getDocumentElement();
         read(root);
         configs.remove(0);
         configs.remove(3);
         configs.remove(6);
         System.out.println(configs);
-        if (configs.get(0) == "true"){
-
+        Basket basket;
+        if (configs.get(0).equals("true")) {
+            switch (configs.get(2)) {
+                case ("json"):
+                    JSONParser parser = new JSONParser();
+                    try {
+                        Object obj = parser.parse(new FileReader(configs.get(1)));
+                        JSONObject jsonObject = (JSONObject) obj;
+                        System.out.println(jsonObject);
+                    } catch (IOException | ParseException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case ("txt"):
+                    basket = Basket.loadFromTxtFile(new File(configs.get(1)));
+                    break;
+                default:
+                    break;
+            }
         }
-
-
         ClientLog clientLog = new ClientLog();
         FileWriter file = new FileWriter("log.csv");
-        Basket basket = new Basket(new String[]{"Молоко", "Хлеб", "Гречневая крупа"}, new int[]{50, 14, 80});
+        basket = new Basket(new String[]{"Молоко", "Хлеб", "Гречневая крупа"}, new int[]{50, 14, 80});
+        basket.setSave(configs.get(3));
+        basket.setSaveFileName(configs.get(4));
+        basket.setSaveFileType(configs.get(5));
         basket.addToCart(0, 2);
         clientLog.log(0, 2);
         basket.addToCart(2, 1);
@@ -46,6 +65,9 @@ public class Main {
             file2.flush();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        if (configs.get(6).equals("true")) {
+            clientLog.exportAsCSV(new FileWriter(configs.get(7)));
         }
         clientLog.exportAsCSV(file);
     }
