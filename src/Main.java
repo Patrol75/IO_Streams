a847000251fc745f88b89bfcc538bcd16fc41639
Maksim Kalashnikov <maksim-kalashnikov-2002@mail.ru>
2023-07-08 21:42:38 +0300

    add XMLReader

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.json.simple.JSONObject;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
+    static List<String> configs = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory. newInstance();
+        DocumentBuilder builder1 = factory.newDocumentBuilder();
+        Document doc = builder1.parse( new File("shop.xml"));
+        Node root = doc.getDocumentElement();
+        read(root);
+        configs.remove(0);
+        configs.remove(3);
+        configs.remove(6);
+        System.out.println(configs);
+        if (configs.get(0) == "true"){
+
+        }
+
+
         ClientLog clientLog = new ClientLog();
         FileWriter file = new FileWriter("log.csv");
         Basket basket = new Basket(new String[]{"Молоко", "Хлеб", "Гречневая крупа"}, new int[]{50, 14, 80});
@@ -25,5 +48,19 @@ public class Main {
             e.printStackTrace();
         }
         clientLog.exportAsCSV(file);
+    }
+
+    private static void read(Node node) {
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                Element element = (Element) node_;
+                NodeList nodeList1 = element.getChildNodes();
+                Node node1 = (Node) nodeList1.item(0);
+                configs.add(node1.getNodeValue());
+                read(node_);
+            }
+        }
     }
 }
